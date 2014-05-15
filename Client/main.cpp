b6bb4ec5913365c82df9d4bd42f8c16e37bcc049
Justin <jyang772@gmail.com>
2014-05-15 10:43:24 -0700

    Coach splash screen

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,11 +1,23 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QSplashScreen>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QSplashScreen *coach = new QSplashScreen;
+    coach->setPixmap(QPixmap("./coach.jpg"));
+    coach->show();
+
+
+
+
     MainWindow w;
     w.show();
+
+    coach->finish(&w);
+    delete coach;
 
     return a.exec();
 }
